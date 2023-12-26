4fb3f275a430316567a8f0a85e0c28e2262e93d3
sssjk <wnsdla529@gmail.com>
2023-12-26 22:21:20 +0900

    231226 투두리스트 작업

--- src/App.js
+++ src/App.js
@@ -13,6 +13,30 @@ export default class App extends Component {
     float: "right"
   }
 
+  getStyle = () => {
+    return {
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: "none"
+    }
+  }
+
+  todoData = [
+    {
+      id: "1",
+      title: "공부하기",
+      completed: true
+    },
+    {
+      id: "2",
+      title: "청소하기",
+      completed: false
+    }
+  ]
+
+  //key={data.id} 리액트에서는 리스트 나열할때 유니크한 값을 써야한다.
+  // {data.title} 로 중괄호를 사용해줘야 title가 나온다.
+
   render() {
     return (
       <duv className="container">
@@ -21,11 +45,14 @@ export default class App extends Component {
             <h1>할 일 목록</h1>
           </div>
 
-          <div>
-            <input type="checkbox" defaultChecked={false}></input>
-            공부하기
-            <button style={this.btnStyle}>x</button>
-          </div>
+          {this.todoData.map(data => (
+            <div style={this.getStyle()} key={data.id}>
+              <input type="checkbox" defaultChecked={data.completed}></input>
+              {data.title}
+              <button style={this.btnStyle}>x</button>
+            </div>
+          ))}
+
 
         </div>
       </duv>
